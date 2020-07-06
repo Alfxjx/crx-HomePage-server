39d2abdd325c9c61b1dcd542e8c607ddf642857f
AlfredPC <xjxtju@163.com>
2020-07-06 17:00:42 +0800

    init infra

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BingModule } from './bing/bing.module';
+import { WeatherModule } from './weather/weather.module';
+import { UserModule } from './user/user.module';
+import { FavoriteModule } from './favorite/favorite.module';
 
 @Module({
-  imports: [],
+  imports: [BingModule, WeatherModule, UserModule, FavoriteModule],
   controllers: [AppController],
   providers: [AppService],
 })
